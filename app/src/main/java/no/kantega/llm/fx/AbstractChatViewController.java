d805156582be742919798e35b8b758d08d7bdd43
Hallvard Trætteberg <hallvard.traetteberg@gmail.com>
2024-04-22 10:52:46 +0200

    Refactored chat views

---
+++ app/src/main/java/no/kantega/llm/fx/AbstractChatViewController.java
@@ -0,0 +1,76 @@
+package no.kantega.llm.fx;
+
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.service.TokenStream;
+import javafx.application.Platform;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextInputControl;
+import no.hal.fx.util.ButtonActionProgressHelper;
+import no.kantega.llm.fx.ChatMemoryViewController.ChatMemoryUpdate;
+
+public abstract class AbstractChatViewController {
+
+    protected abstract TextInputControl getAiMessageTextControl();
+    
+    protected abstract ChatMemory getChatMemory();
+
+    private Property<ChatMemoryUpdate> chatMemoryUpdateProperty = new SimpleObjectProperty<ChatMemoryUpdate>();
+    
+    protected ObservableValue<ChatMemoryUpdate> chatMemoryUpdateProperty() {
+        return chatMemoryUpdateProperty;
+    }
+
+    protected abstract ChatbotAgent getChatbotAgent();
+
+    private ButtonActionProgressHelper buttonActionProgressHelper = new ButtonActionProgressHelper();
+
+    protected void chatMemoryUpdated(String nextToken) {
+        chatMemoryUpdateProperty.setValue(new ChatMemoryUpdate(getChatMemory(), nextToken));
+    }
+
+    protected void handleNextAiMessageToken(String nextToken) {
+        if (getAiMessageTextControl().getText().isEmpty()) {
+            chatMemoryUpdated(null);
+        }
+        if (getAiMessageTextControl() instanceof TextArea textArea) {
+            textArea.appendText(nextToken);
+            //textArea.setScrollTop(Double.MAX_VALUE);
+        } else {
+            getAiMessageTextControl().setText(getAiMessageTextControl().getText() + nextToken);
+        }
+        chatMemoryUpdated(nextToken);
+    }
+
+    protected void handleCompleteAiMessage(AiMessage aiMessage) {
+        getAiMessageTextControl().setText(aiMessage.text());
+        if (getAiMessageTextControl() instanceof TextArea textArea) {
+            textArea.setScrollTop(Double.MAX_VALUE);
+        }
+        chatMemoryUpdated(null);
+    }
+
+    interface ChatbotAgent {
+        TokenStream answer(String query);
+    }
+
+    void handleSendUserMessage(String userMessage, Object eventSource) {
+        getAiMessageTextControl().setText("");
+        buttonActionProgressHelper.performStreamingAction(eventSource, callback ->
+            getChatbotAgent().answer(userMessage)
+                .onNext(nextToken -> {
+                    callback.call(null);
+                    Platform.runLater(() -> handleNextAiMessageToken(nextToken));
+                })
+                .onComplete(answer -> {
+                    callback.call(true);
+                    Platform.runLater(() -> handleCompleteAiMessage(answer.content()));
+                })
+                .onError(throwable -> callback.call(false))
+                .start()
+            );
+    }
+}
--- app/src/main/java/no/kantega/llm/fx/SimpleChatViewController.java
+++ app/src/main/java/no/kantega/llm/fx/SimpleChatViewController.java
@@ -5,33 +5,44 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.StreamingChatLanguageModel;
 import dev.langchain4j.service.AiServices;
-import dev.langchain4j.service.TokenStream;
 import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
-import javafx.application.Platform;
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextInputControl;
 import no.hal.fx.adapter.CompositeLabelAdapter;
 import no.hal.fx.adapter.LabelAdapter;
 import no.hal.fx.bindings.BindableView;
 import no.hal.fx.bindings.BindingSource;
 import no.hal.fx.bindings.BindingTarget;
-import no.hal.fx.util.ButtonActionProgressHelper;
 import no.kantega.llm.fx.ChatMemoryViewController.ChatMemoryUpdate;
 
 @Dependent
-public class SimpleChatViewController implements BindableView {
+public class SimpleChatViewController extends AbstractChatViewController implements BindableView {
+
+    private ChatMemory chatMemory;
+
+    @Override
+    protected ChatMemory getChatMemory() {
+        return chatMemory;
+    }
+
+    private ChatbotAgent chatbotAgent;
+
+    @Override
+    protected ChatbotAgent getChatbotAgent() {
+        return chatbotAgent;
+    }
 
     @FXML
     TextInputControl systemPromptText;
@@ -45,19 +56,16 @@ public class SimpleChatViewController implements BindableView {
     @FXML
     TextInputControl aiMessageText;
 
+    @Override
+    protected TextInputControl getAiMessageTextControl() {
+        return aiMessageText;
+    }
+
     @Inject
     Instance<LabelAdapter> labelAdapters;
 
     private Property<StreamingChatLanguageModel> chatModelProperty = new SimpleObjectProperty<StreamingChatLanguageModel>();
-    
-    private ChatMemory chatMemory;
-
-    private Property<ChatMemoryUpdate> chatMemoryUpdateProperty = new SimpleObjectProperty<ChatMemoryUpdate>();
-
-    private ChatbotAgent chatbotAgent;
-
-    private ButtonActionProgressHelper buttonActionProgressHelper;
-    
+        
     private String systemPrompt = """
         You are a simple chatbot.
         """;
@@ -97,8 +105,6 @@ public class SimpleChatViewController implements BindableView {
                 );
             }
         });
-
-        buttonActionProgressHelper = new ButtonActionProgressHelper();
         systemPromptText.textProperty().subscribe(text -> handleRestartChat());
 
         String sendUserMessageActionTextFormat = sendUserMessageAction.getText();
@@ -111,47 +117,19 @@ public class SimpleChatViewController implements BindableView {
             new BindingTarget<StreamingChatLanguageModel>(sendUserMessageAction, StreamingChatLanguageModel.class, chatModelProperty)
         );
         bindingSources = List.of(
-            new BindingSource<ChatMemoryUpdate>(this.aiMessageText, ChatMemoryUpdate.class, chatMemoryUpdateProperty)
+            new BindingSource<ChatMemoryUpdate>(this.aiMessageText, ChatMemoryUpdate.class, chatMemoryUpdateProperty())
         );
-    }
-
-    private void chatMemoryUpdated(String nextToken) {
-        chatMemoryUpdateProperty.setValue(new ChatMemoryUpdate(chatMemory, nextToken));
     }
 
     @FXML
     void handleSendUserMessage(ActionEvent event) {
-        aiMessageText.setText("");
-        buttonActionProgressHelper.performStreamingAction(event.getSource(), callback ->
-            chatbotAgent.answer(userMessageText.getText())
-                .onNext(
-                    nextToken -> {
-                        callback.call(null);
-                        Platform.runLater(() -> {
-                            if (aiMessageText.getText().isEmpty()) {
-                                chatMemoryUpdated(null);
-                            }
-                            aiMessageText.setText(aiMessageText.getText() + nextToken);
-                            if (aiMessageText instanceof TextArea textArea) {
-                                textArea.setScrollTop(Double.MAX_VALUE);
-                            }
-                            chatMemoryUpdated(nextToken);
-                        });
-                    }    
-                )
-                .onComplete(
-                    answer -> {
-                        callback.call(true);
-                        Platform.runLater(() -> {
-                            userMessageText.setText(null);
-                            aiMessageText.setText(answer.content().text());
-                            chatMemoryUpdated(null);
-                        });
-                    }
-                )
-                .onError(throwable -> callback.call(false))
-                .start()
-            );
+        handleSendUserMessage(userMessageText.getText(), event.getSource());
+    }
+
+    @Override
+    protected void handleCompleteAiMessage(AiMessage aiMessage) {
+        userMessageText.setText(null);
+        super.handleCompleteAiMessage(aiMessage);
     }
 
     @FXML
@@ -159,9 +137,5 @@ public class SimpleChatViewController implements BindableView {
         chatMemory.clear();
         chatMemory.add(new SystemMessage(systemPromptText.getText()));
         chatMemoryUpdated(null);
-    }
-
-    interface ChatbotAgent {
-        TokenStream answer(String query);
     }
 }
